2026589994932c142eedb582ad6fb2cc8f25301b
codewithsathwik <sathvikms344@gmail.com>
2025-10-15 00:03:10 +0530

    made some changes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 let searchBtn = document.querySelector("#search-btn");
+let cityHead = document.querySelector("#location");
+let weather = document.querySelector("#weather");
+let description = document.querySelector("#description");
 
 
 searchBtn.addEventListener("click", () => {
     let cityName = document.querySelector("#cityName").value.trim();
-    if (!cityName) {
-        return;
-    }
+    if (!cityName) return;
     document.querySelector("#enter-msg").classList.add("hidden");
 });
 
